37c9f1472c61463414bc3ab5cc2a86536c86cd93
Kolecko2004 <dr.vojtisek007@gmail.com>
2025-11-29 15:55:23 +0100

    First commit, start of header, hero and footer. Lets go

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./main/**/*.html",
+    "./main/components/**/*.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: "#eee9df",
+          default: "#2c3b4d",
+          spark: "#ffb162",
+          accent: "#a35139",
+          dark: "1b2632",
+        },
+        
+      },
+    },
+  },
+  plugins: [],
+};
